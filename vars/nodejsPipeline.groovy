342389cc22ed256dfba9571e7b7ea4c4cbaa5bbd
Satyam Pandey <93024366+Satyam039@users.noreply.github.com>
2026-09-16 15:22:38 +0530

    Update nodejsPipeline.groovy

--- vars/nodejsPipeline.groovy
+++ vars/nodejsPipeline.groovy
@@ -16,23 +16,19 @@ def call(Map config = [:]) {
             disableConcurrentBuilds()
         }
 
-     stage('Checkout') {
-    steps {
-        withCredentials([usernamePassword(
-            credentialsId: 'github-creds',
-            usernameVariable: 'GIT_USERNAME',
-            passwordVariable: 'GIT_TOKEN'
-        )]) {
-            sh '''
-                rm -rf ./*
-                git clone \
-                  --branch main \
-                  --single-branch \
-                  https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/Satyam039/website-downtime-alert.git .
-            '''
-        }
-    }
-}
+        stages {
+
+            stage('Checkout') {
+                steps {
+                    sh '''
+                        rm -rf ./*
+                        git -c credential.helper= clone \
+                          --branch main \
+                          --single-branch \
+                          https://github.com/Satyam039/website-downtime-alert.git .
+                    '''
+                }
+            }
 
             stage('Environment') {
                 steps {
