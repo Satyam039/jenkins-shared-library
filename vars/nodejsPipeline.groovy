c740058e0f67c1fc9559da9347f5689eb9dbb827
Satyam Pandey <93024366+Satyam039@users.noreply.github.com>
2026-09-16 14:38:32 +0530

    Create nodejsPipeline.groovy

---
+++ vars/nodejsPipeline.groovy
@@ -0,0 +1,76 @@
+def call(Map config = [:]) {
+
+    def appName = config.get('appName', 'node-app')
+
+    pipeline {
+
+        agent any
+
+        environment {
+            APP_NAME = "${appName}"
+        }
+
+        options {
+            timestamps()
+            timeout(time: 30, unit: 'MINUTES')
+            disableConcurrentBuilds()
+        }
+
+        stages {
+
+            stage('Checkout') {
+                steps {
+                    checkout scm
+                }
+            }
+
+            stage('Environment') {
+                steps {
+                    sh '''
+                        echo "Application: $APP_NAME"
+                        node --version
+                        npm --version
+                    '''
+                }
+            }
+
+            stage('Install') {
+                steps {
+                    sh 'npm ci'
+                }
+            }
+
+            stage('Lint') {
+                steps {
+                    sh 'npm run lint --if-present'
+                }
+            }
+
+            stage('Test') {
+                steps {
+                    sh 'npm test --if-present'
+                }
+            }
+
+            stage('Build') {
+                steps {
+                    sh 'npm run build --if-present'
+                }
+            }
+        }
+
+        post {
+            success {
+                echo "BUILD SUCCESS: ${APP_NAME}"
+            }
+
+            failure {
+                echo "BUILD FAILED: ${APP_NAME}"
+            }
+
+            always {
+                cleanWs()
+            }
+        }
+    }
+}
