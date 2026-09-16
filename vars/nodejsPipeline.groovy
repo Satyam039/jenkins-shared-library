c872ebbe5de3f1099c18eb33819732173a93872d
Satyam Pandey <93024366+Satyam039@users.noreply.github.com>
2026-09-16 15:30:04 +0530

    Update nodejsPipeline.groovy

--- vars/nodejsPipeline.groovy
+++ vars/nodejsPipeline.groovy
@@ -18,32 +18,11 @@ def call(Map config = [:]) {
 
         stages {
 
-            stage('Checkout') {
-                steps {
-                    sh '''
-                        rm -rf ./*
-
-                        curl -L \
-                          https://github.com/Satyam039/website-downtime-alert/archive/refs/heads/main.tar.gz \
-                          -o repo.tar.gz
-
-                        tar -xzf repo.tar.gz --strip-components=1
-
-                        rm -f repo.tar.gz
-
-                        echo "Repository downloaded successfully"
-                        ls -la
-                    '''
-                }
-            }
-
             stage('Environment') {
                 steps {
                     sh '''
                         echo "Application: $APP_NAME"
-                        echo "Node Version:"
                         node --version
-                        echo "NPM Version:"
                         npm --version
                     '''
                 }
@@ -75,7 +54,6 @@ def call(Map config = [:]) {
         }
 
         post {
-
             success {
                 echo "BUILD SUCCESS: ${APP_NAME}"
             }
