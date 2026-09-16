f572fc6eaef3ecb47d6bbd2b1993d3c807efc83d
Satyam Pandey <93024366+Satyam039@users.noreply.github.com>
2026-09-16 15:24:44 +0530

    Update nodejsPipeline.groovy

--- vars/nodejsPipeline.groovy
+++ vars/nodejsPipeline.groovy
@@ -16,19 +16,40 @@ def call(Map config = [:]) {
             disableConcurrentBuilds()
         }
 
-        stages {
+       stage('Checkout') {
+    steps {
+        withCredentials([
+            usernamePassword(
+                credentialsId: 'github-creds',
+                usernameVariable: 'GIT_USERNAME',
+                passwordVariable: 'GIT_TOKEN'
+            )
+        ]) {
+            sh '''
+                rm -rf ./*
 
-            stage('Checkout') {
-                steps {
-                    sh '''
-                        rm -rf ./*
-                        git -c credential.helper= clone \
-                          --branch main \
-                          --single-branch \
-                          https://github.com/Satyam039/website-downtime-alert.git .
-                    '''
-                }
-            }
+                export GIT_ASKPASS="$WORKSPACE/git-askpass.sh"
+
+                cat > "$GIT_ASKPASS" <<'EOF'
+#!/bin/sh
+case "$1" in
+    *Username*) echo "$GIT_USERNAME" ;;
+    *Password*) echo "$GIT_TOKEN" ;;
+esac
+EOF
+
+                chmod +x "$GIT_ASKPASS"
+
+                git -c credential.helper= clone \
+                  --branch main \
+                  --single-branch \
+                  https://github.com/Satyam039/website-downtime-alert.git .
+
+                rm -f "$GIT_ASKPASS"
+            '''
+        }
+    }
+}
 
             stage('Environment') {
                 steps {
